3493a9cdacea454422a49c5d8b8ca914a5160b14
jinomorales <jino0000morales0000@gmail.com>
2021-12-06 15:59:54 -0500

    optimizacion seo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,21 +4,43 @@ namespace App\Http\Controllers;
 
 use App\Mail\ContatoMail;
 use Illuminate\Support\Facades\Mail;
+use SEO;
 
 class HomeController extends Controller
 {
     public function index()
     {
+        SEO::setTitle('Tensooh Media');
+        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.!');
+        SEO::opengraph()->setUrl('http://tensooh.com');
+        SEO::setCanonical('https://tensooh.com');
+        SEO::opengraph()->addProperty('type', 'articles');
+        SEO::twitter()->setSite('@tensooh');
+
         return view('welcome');
     }
 
     public function about()
     {
+        SEO::setTitle('Tensooh Media');
+        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.!');
+        SEO::opengraph()->setUrl('http://tensooh.com');
+        SEO::setCanonical('https://tensooh.com');
+        SEO::opengraph()->addProperty('type', 'articles');
+        SEO::twitter()->setSite('@tensooh');
+
         return view('aboutus');
     }
 
     public function contact()
     {
+        SEO::setTitle('Tensooh Media');
+        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.!');
+        SEO::opengraph()->setUrl('http://tensooh.com');
+        SEO::setCanonical('https://tensooh.com');
+        SEO::opengraph()->addProperty('type', 'articles');
+        SEO::twitter()->setSite('@tensooh');
+
         return view('contacto');
     }
 
--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -5,10 +5,12 @@
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title> @yield('title')</title>
-    <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
     <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+
+    {!! SEO::generate() !!}
+
 
     <!-- CSS here -->
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
