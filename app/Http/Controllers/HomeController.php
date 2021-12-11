d44b2a8f7e7968a21eee6a2ed5851da13d0b24f4
jinomorales <jino0000morales0000@gmail.com>
2021-12-11 00:59:53 -0500

    seo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,7 +12,13 @@ class HomeController extends Controller
     public function index()
     {
         SEO::setTitle('Tensooh Media');
-        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.!');
+        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.! Durante 15 años hemos realizado grandes proyectos de la mano de nuestros clientes, a
+        quienes agradecemos su confianza y compañía durante todo este tiempo.
+        Estamos comprometidos con la realización de trabajos con los más altos estándares de
+        calidad en la publicidad OOH, caracterizándonos por ser una empresa sólida y con
+        experiencia en el mercado nacional.
+        Creamos nuestras propias estructuras con un equipo de creativos diseñadores,
+        departamento de logística, impresión y metal mecánica.');
         SEO::opengraph()->setUrl('http://tensooh.com');
         SEO::setCanonical('https://tensooh.com');
         SEO::opengraph()->addProperty('type', 'articles');
@@ -24,7 +30,13 @@ class HomeController extends Controller
     public function about()
     {
         SEO::setTitle('Tensooh Media');
-        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.!');
+        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.! Durante 15 años hemos realizado grandes proyectos de la mano de nuestros clientes, a
+        quienes agradecemos su confianza y compañía durante todo este tiempo.
+        Estamos comprometidos con la realización de trabajos con los más altos estándares de
+        calidad en la publicidad OOH, caracterizándonos por ser una empresa sólida y con
+        experiencia en el mercado nacional.
+        Creamos nuestras propias estructuras con un equipo de creativos diseñadores,
+        departamento de logística, impresión y metal mecánica.');
         SEO::opengraph()->setUrl('http://tensooh.com');
         SEO::setCanonical('https://tensooh.com');
         SEO::opengraph()->addProperty('type', 'articles');
@@ -36,7 +48,13 @@ class HomeController extends Controller
     public function contact()
     {
         SEO::setTitle('Tensooh Media');
-        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.!');
+        SEO::setDescription('Somos Más Que Una Empresa, Somos Una Familia.! Durante 15 años hemos realizado grandes proyectos de la mano de nuestros clientes, a
+        quienes agradecemos su confianza y compañía durante todo este tiempo.
+        Estamos comprometidos con la realización de trabajos con los más altos estándares de
+        calidad en la publicidad OOH, caracterizándonos por ser una empresa sólida y con
+        experiencia en el mercado nacional.
+        Creamos nuestras propias estructuras con un equipo de creativos diseñadores,
+        departamento de logística, impresión y metal mecánica.');
         SEO::opengraph()->setUrl('http://tensooh.com');
         SEO::setCanonical('https://tensooh.com');
         SEO::opengraph()->addProperty('type', 'articles');
